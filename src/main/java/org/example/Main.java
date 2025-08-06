e469a19c2a18ce47215dc4bad3eb316122aedf40
Dmitriy Sim <osim57@mail.ru>
2025-08-06 23:14:38 +0500

    Added first method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Is anybody home???");
+        System.out.println(createMessage ());
+    }
+
+    private static String createMessage (){
+        return "Сообщение для возвращения";
     }
 }
