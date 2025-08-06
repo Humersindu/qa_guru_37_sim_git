53c130e6ef60eea258e76956d98c1b5de48ff17c
Dmitriy Sim <osim57@mail.ru>
2025-08-06 21:36:46 +0500

    change message in a bottle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Лера, драйверы потом");
+        System.out.println("Is anybody home???");
     }
 }
