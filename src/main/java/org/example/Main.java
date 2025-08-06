ae58c285459a857c399d98f805b33d08f64d19d1
Dmitriy Sim <87942624+Humersindu@users.noreply.github.com>
2025-08-06 23:12:54 +0500

    Update Main.java
    Add some messages

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Is anybody home???");
+        System.out.print("No, isn't");
+        System.out.println("But it's a just question!");
     }
 }
