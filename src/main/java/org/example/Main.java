dfd2a76e7c75daed53132676247ea4cd6bf91af9
Dmitriy Sim <87942624+Humersindu@users.noreply.github.com>
2025-08-06 23:25:44 +0500

    Update Main.java
    Add variable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Is anybody home???");
+        String msg1 = "Is anybody home???";
+        
+        System.out.println(msg1);
         System.out.print("No, isn't");
         System.out.println("But it's a just question!");
     }
